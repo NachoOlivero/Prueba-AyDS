d8f28ae1a2a38d8defe2dec325de3303d5da9f4f
aitorortunio <43756956+aitorortunio@users.noreply.github.com>
2021-03-31 18:30:40 -0300

    Update ClasePrueba.java

--- Prueba/src/ClasePrueba.java
+++ Prueba/src/ClasePrueba.java
@@ -3,7 +3,7 @@ public class ClasePrueba {
 	
 	public static void main ( String args[] ) {
 		
-			System.out.println("Hola, mi nombre es"+" Nacho");
+			System.out.println("Hola, mi nombre es"+" Aitor");
 		
 	}
 
