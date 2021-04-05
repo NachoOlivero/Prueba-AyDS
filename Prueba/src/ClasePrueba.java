5df09d2252fbb6c3ee842937a4a4147a7732a2c0
aitorortunio <43756956+aitorortunio@users.noreply.github.com>
2021-04-05 09:50:57 -0300

    nombre Aitor

--- Prueba/src/ClasePrueba.java
+++ Prueba/src/ClasePrueba.java
@@ -1,10 +1,10 @@
 
 public class ClasePrueba {
-	
+
 	public static void main ( String args[] ) {
-		
-			System.out.println("Hola, mi nombre es"+" Nacho");
-		
+
+			System.out.println("Hola, mi nombre es"+" Aitor");
+
 	}
 
 }
